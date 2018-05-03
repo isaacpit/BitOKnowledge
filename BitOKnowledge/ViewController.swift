d0f6e237cae6324f157af185bc98eb6d124cd993
isaacpitblado <isaacpitblado@tamu.edu>
2018-05-03 15:07:48 -0500

    Initial Commit

---
+++ BitOKnowledge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitOKnowledge
+//
+//  Created by Isaac on 5/3/18.
+//  Copyright © 2018 Isaac Pitblado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
